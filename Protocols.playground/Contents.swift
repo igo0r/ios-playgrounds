b8444e8795ba47aca4a7326e7f479c0e31e331b4
Igor Lantushenko <igla@ciklum.com>
2017-03-05 18:16:01 +0200

    added protocol extension examples

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -4,76 +4,68 @@ import UIKit
 
 protocol Vehicle: CustomStringConvertible {
     var isRunning: Bool { get set }
+    var make: String {get set}
+    var model: String {get set}
     mutating func start()
     mutating func turnOff()
 }
 
-struct SportsCar: Vehicle {
+extension Vehicle {
+    var makeModel: String {
+        return "\(make) \(model)"
+    }
     
-
+    mutating func start() {
+        if isRunning {
+            print("Already running!!111")
+        } else {
+            isRunning = true
+            print("\(self.description)")
+        }
+    }
+    
     mutating func turnOff() {
         if isRunning {
             isRunning = false
-            print("Stop it!!")
+            print("\(self.description) shutdown")
         } else {
-            print("already stopped")
+            print("already stopped!!!11")
         }
     }
+}
 
-    mutating func start() {
-        if isRunning {
-            print("Already running!!")
-        } else {
-            isRunning = true
-            print("Lets start it")
-        }
-    }
-
+struct SportsCar: Vehicle {
+    
+    var make: String
+    var model: String
     var isRunning: Bool = false
     var description: String {
-        if isRunning {
-            return "SportsCar currently running"
-        } else {
-            return "Not running!!11"
-        }
+        return self.makeModel
     }
 }
 
 class SemiTruck: Vehicle {
     var isRunning: Bool = false
+    var make: String
+    var model: String
+    
+    init(isRunning: Bool, make: String, model: String) {
+        self.isRunning = isRunning
+        self.make = make
+        self.model = model
+    }
     var description: String {
-        if isRunning {
-            return "Semitruck currently running"
-        } else {
-            return "Semitruck shutdown"
-        }
+        return self.makeModel
     }
     
-    func turnOff() {
-        if isRunning {
-            isRunning = false
-            print("Lets stop it")
-        } else {
-            print("Already stopped")
-        }
-    }
-    
-    func start() {
-        if isRunning {
-            print("Already started")
-        } else {
-            isRunning = true
-            print("Lets start it")
-        }
-    }
-    
+
     func blowAirHorn() {
         print("TOOOOOTOOOO!!!")
     }
 }
 
-var car1 = SportsCar()
-var truck1 = SemiTruck()
+var car1 = SportsCar(make: "Porsche", model: "911", isRunning: false)
+var truck1 = SemiTruck(isRunning: false, make: "Mercedes", model: "Lucia")
 
 car1.start()
 truck1.start()
@@ -83,8 +75,17 @@ truck1.turnOff()
 
 var vehicles: [Vehicle] = [car1, truck1]
 for vehicle in vehicles {
-    print(vehicle)
+    print(vehicle.makeModel)
     if let semi = vehicle as? SemiTruck {
         semi.blowAirHorn()
     }
 }
+
+extension Double {
+    var dollarString: String {
+        return String(format: "$%.02f", self)
+    }
+}
+
+var test = 23.45 * 0.23
+print (test.dollarString)
