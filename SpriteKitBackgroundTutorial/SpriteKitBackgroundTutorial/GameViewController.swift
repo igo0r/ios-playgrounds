0d7acc33406d7f449412a5534370cff92a6cb6bf
Igor Lantushenko <igla@ciklum.com>
2017-04-02 10:40:18 +0300

    added spritekit tutorial

---
+++ SpriteKitBackgroundTutorial/SpriteKitBackgroundTutorial/GameViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GameViewController.swift
+//  SpriteKitBackgroundTutorial
+//
+//  Created by Igor Lantushenko on 02/04/2017.
+//  Copyright © 2017 Igor Lantushenko. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: CGSize(width: 1080, height: 1920))
+        
+        if let view = self.view as! SKView? {
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
